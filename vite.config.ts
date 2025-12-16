4399feb6dcec8156056ab1bebc08cb9df0afa4a9
yassklm <yassinekelmo@gmail.com>
2025-12-16 14:35:48 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
-import path from 'path';
-import { defineConfig, loadEnv } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, '.', '');
+export default defineConfig({
+  base: '/NoorAlHuda/', // ⚠️ خاصها تكون نفس اسم repo بالضبط
+  plugins: [react()],
+})
 
   return {
     base: '/NoorAlHuda/', // 🔴 VERY IMPORTANT (repo name)
